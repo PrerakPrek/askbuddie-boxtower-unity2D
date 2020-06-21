99c02562cee7a237424cf1275204f839ab7c969d
Prerak <Prerak@users.noreply.github.com>
2020-06-22 02:58:27 +0545

    First Commit
    -Added Project Files

---
+++ Assets/Scripts/CamerFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerFollow : MonoBehaviour
+{
+
+    public Vector3 targetPos;
+
+    private float smoothmove = 1f;
+    void Start()
+    {
+        targetPos = transform.position;
+        targetPos.z = -10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,targetPos,smoothmove*Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public BoxSpawner boxSpawenr;
+    [HideInInspector]
+    public BoxScript boxScript;
+    public GameObject GameOverScreen;
+    public CamerFollow camScript;
+    private int moveCount;
+    void Awake()
+    {
+        if(instance==null){
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        boxSpawenr.SpawnBox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            boxScript.ReleaseBox();
+        }
+    }
+
+    public void SpawnNextBox()
+    {
+        Invoke("NextBox",0f);
+    }
+
+    void NextBox()
+    {
+        boxSpawenr.SpawnBox();
+    }
+    public void moveCamera()
+    {
+        moveCount++;
+        if(moveCount==4){
+            moveCount = 0;
+            camScript.targetPos.y += 4f;
+        }
+    }
+    public void RestartGame()
+    {
+        GameOverScreen.SetActive(true);
+        //Application.LoadLevel(0);
+    }
+
+
+   
+
+
+
+}
